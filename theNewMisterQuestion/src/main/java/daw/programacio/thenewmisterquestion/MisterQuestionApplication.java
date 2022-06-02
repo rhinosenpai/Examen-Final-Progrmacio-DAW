49f3a28711073e997c3e0c23115fca5f648955e4
daw1 <matacastello@gmail.com>
2022-06-02 20:43:36 +0200

    a

--- theNewMisterQuestion/src/main/java/daw/programacio/thenewmisterquestion/MisterQuestionApplication.java
+++ theNewMisterQuestion/src/main/java/daw/programacio/thenewmisterquestion/MisterQuestionApplication.java
@@ -10,13 +10,15 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class MisterQuestionApplication extends Application {
+    public static Stage window;
+
     @Override
     public void start(Stage stage) throws IOException {
         if(DBFacade.logOn()) {
             System.out.println("Connexion established");
             DBFacade.select();
         }
-
+        window = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(MisterQuestionApplication.class.getResource("views/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
--- theNewMisterQuestion/src/main/java/daw/programacio/thenewmisterquestion/controllers/MainController.java
+++ theNewMisterQuestion/src/main/java/daw/programacio/thenewmisterquestion/controllers/MainController.java
@@ -1,14 +1,31 @@
 package daw.programacio.thenewmisterquestion.controllers;
 
+import daw.programacio.thenewmisterquestion.MisterQuestionApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onbuttonclick() {
+        FXMLLoader loader = new FXMLLoader(MisterQuestionApplication.class.getResource("views/questions-view.fxml"));
+
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        MisterQuestionApplication.window.setScene(scene);
+        MisterQuestionApplication.window.show();
     }
 }
